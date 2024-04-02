22c975747c3324fd49a2ccf90936b6dda61ee5ee
Twinkle Pardeshi <65865151+Twinkle110@users.noreply.github.com>
2024-04-02 19:04:34 +0530

    Update PDF_GPT.py

--- PDF_GPT.py
+++ PDF_GPT.py
@@ -8,7 +8,6 @@ from langchain.vectorstores import FAISS
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
 from langchain.chains.question_answering import load_qa_chain
-#from langchain import PromptTemplate
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationalRetrievalChain
 from langchain_core.prompts import PromptTemplate
@@ -20,8 +19,6 @@ from langchain.schema import(
     HumanMessage,
     SystemMessage,
 )
-
-
 
 css = '''
 <style>
@@ -68,7 +65,6 @@ user_template = '''
     <div class="message">{{MSG}}</div>
 </div>
 '''
-
 system_message_content = "You are an intelligent AI which analyses text from documents and answers the user's questions. Please answer in as much detail as possible, so that the user does not have to revisit the document. If you don't know the answer, say that you don't know, and avoid making up things."
 
 def get_pdf_text(pdf_docs):
@@ -78,8 +74,6 @@ def get_pdf_text(pdf_docs):
         for page in pdf_reader.pages:
             text += page.extract_text()
     return text
-
-
 
 def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
@@ -91,16 +85,13 @@ def get_text_chunks(text):
     chunks = text_splitter.split_text(text)
     return chunks
 
-
 def get_vectorstore(text_chunks):
     embeddings = OpenAIEmbeddings()
     # embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
 
-
 def get_conversation_chain(vectorstore):
-
     prompt_template = """
     You are an intelligent AI which analyses text from pdf, pdfs and answers the user's questions. Please answer in as much detail as possible, so that the user does not have to 
     revisit the document. If you don't know the answer, say that you don't know, and avoid making up things.Please provide output in 4 sections.\n\n
@@ -112,44 +103,41 @@ def get_conversation_chain(vectorstore):
     llm = ChatOpenAI(
             #model_name="gpt-3.5-turbo",
             model_name="gpt-4-1106-preview",
-            
-            
     )
     # llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
     
-    PROMPT = PromptTemplate(template = prompt_template, input_variables = ["context", "question"])
+    PROMPT = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
     
-    memory = ConversationBufferMemory(
-    memory_key='chat_history', return_messages=True)
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
     
-    qgc = LLMChain(llm=llm,prompt=PROMPT)
+    qgc = LLMChain(llm=llm, prompt=PROMPT)
     
     conversation_chain = ConversationalRetrievalChain.from_llm(
         llm=llm,
         retriever=vectorstore.as_retriever(),
         memory=memory,
-        
     )
 
     return conversation_chain
 
 def handle_userinput(user_question):
-    response = st.session_state.conversation({'question': user_question})
-    st.session_state.chat_history = response['chat_history']
+    if st.session_state.conversation is None:
+        st.write("Please click on the 'Process' button")
+    else:
+        response = st.session_state.conversation({'question': user_question})
+        st.session_state.chat_history = response['chat_history']
 
-    for i, message in enumerate(st.session_state.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-        else:
-            st.write(bot_template.replace(
-                "{{MSG}}", message.content), unsafe_allow_html=True)
-
+        for i, message in enumerate(st.session_state.chat_history):
+            if i % 2 == 0:
+                st.write(user_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
+            else:
+                st.write(bot_template.replace(
+                    "{{MSG}}", message.content), unsafe_allow_html=True)
 
 def app():
     load_dotenv()
-    #st.set_page_config(page_title="Chat with multiple PDFs",
-                       #page_icon=":books:")
+    #st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
@@ -159,31 +147,27 @@ def app():
 
     # st.header("Chat with your document :books:")
     
-
     # with st.sidebar:
     st.subheader("Chat with your document :books:")
-    pdf_docs = st.file_uploader("Upload your PDFs here and click process button", type=["pdf"],accept_multiple_files=True)
+    pdf_docs = st.file_uploader("Upload your PDFs here'", type=["pdf"], accept_multiple_files=True)
         
     if st.button("Process"):
-            with st.spinner("Processing"):
-                # get pdf text
-                 raw_text = get_pdf_text(pdf_docs)
+        with st.spinner("Processing"):
+            # get pdf text
+            raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks
-                 text_chunks = get_text_chunks(raw_text)
+            # get the text chunks
+            text_chunks = get_text_chunks(raw_text)
 
-                
-                # create vector store
-                 vectorstore = get_vectorstore(text_chunks)
+            # create vector store
+            vectorstore = get_vectorstore(text_chunks)
 
-                # create conversation chain
-                 st.session_state.conversation = get_conversation_chain(
-                vectorstore)
+            # create conversation chain
+            st.session_state.conversation = get_conversation_chain(vectorstore)
     
     user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
-
 
 if __name__ == '__main__':
     app()
